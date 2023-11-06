652c244409a0834a29c324c90239f8014f1a8099
anaspacheco <asp9060@nyu.edu>
2023-11-06 16:46:34 -0500

    Revert "implemented function and tests"

---
+++ src/harry_potter/cretate_wizard.py
@@ -0,0 +1,26 @@
+
+
+class CreateWizard:
+    
+    
+    # type indicates good or bad
+    def wizardNameGenerator(self,type):
+        pass
+
+    def pickName(slef):
+        pass
+
+    def chooseCandy(self):
+        pass
+
+    def chooseAnimal(self):
+        pass
+
+    def chooseHouse(self):
+        pass
+
+    
+
+
+
+
