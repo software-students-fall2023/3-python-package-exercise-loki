64660b9c1b4d59cb7caceefadb636874bda55909
anaspacheco <asp9060@nyu.edu>
2023-11-06 16:41:08 -0500

    implemented function and tests

---
+++ src/harry_potter/house_quiz_questions.py
@@ -0,0 +1,63 @@
+house_questions = [
+    "\nWhat trait do you value most in yourself?\n"
+    "a. Courage\n"
+    "b. Loyalty\n"
+    "c. Intelligence\n"
+    "d. Ambition",
+
+    "\nIn a difficult situation, what is your instinctive reaction?\n"
+    "a. Face it head-on\n"
+    "b. Seek help from friends\n"
+    "c. Analyze and strategize\n"
+    "d. Find the most advantageous solution",
+
+    "\nWhat kind of people do you admire?\n"
+    "a. Brave and daring\n"
+    "b. Loyal and true\n"
+    "c. Intelligent and wise\n"
+    "d. Ambitious and determined",
+
+    "\nHow do you approach learning?\n"
+    "a. Through experience\n"
+    "b. Collaboratively with others\n"
+    "c. By analyzing and studying\n"
+    "d. Through strategic planning",
+
+    "\nWhich animal represents you best?\n"
+    "a. Lion\n"
+    "b. Badger\n"
+    "c. Eagle\n"
+    "d. Serpent",
+
+    "\nHow do you handle pressure?\n"
+    "a. Confront it directly\n"
+    "b. Rely on your support system\n"
+    "c. Think strategically to find solutions\n"
+    "d. Use it as motivation to succeed",
+
+    "\nWhat type of magical creature fascinates you the most?\n"
+    "a. Dragon\n"
+    "b. Hippogriff\n"
+    "c. Phoenix\n"
+    "d. Basilisk",
+
+    "\nWhat is your favorite school subject?\n"
+    "a. Defense Against the Dark Arts\n"
+    "b. Herbology\n"
+    "c. Potions\n"
+    "d. Transfiguration",
+
+    "\nWhat kind of magical spell would you prefer to learn?\n"
+    "a. A spell for protection and defense\n"
+    "b. A spell for healing and nurturing\n"
+    "c. A spell for knowledge and discovery\n"
+    "d. A spell for manipulation and control",
+
+    "\nWhich Hogwarts teacher do you find most interesting?\n"
+    "a. Professor McGonagall\n"
+    "b. Professor Sprout\n"
+    "c. Professor Flitwick\n"
+    "d. Professor Snape",
+] 
+
+
